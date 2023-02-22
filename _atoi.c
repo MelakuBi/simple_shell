1198fb4b3fcc301006fa09c57312d6b96ce4d361
Melaku Birhanu <Melakuirhanu@gmail.com>
2023-02-22 10:52:40 -0800

    update atoi

--- _atoi.c
+++ _atoi.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
-int interactive(info_t *info)
+	int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
@@ -17,11 +17,11 @@ int interactive(info_t *info)
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+	int is_delim(char c, char *delim)
 {
 	while (*delim)
-		if (*delim++ == c)
-			return (1);
+	if (*delim++ == c)
+	return (1);
 	return (0);
 }
 
@@ -30,46 +30,41 @@ int is_delim(char c, char *delim)
  *@c: The character to input
  *Return: 1 if c is alphabetic, 0 otherwise
  */
-
-int _isalpha(int c)
+	int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
+	return (1);
 	else
-		return (0);
+	return (0);
 }
-
 /**
  *_atoi - converts a string to an integer
  *@s: the string to be converted
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *s)
+	int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
 	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
+{
+	if (s[i] == '-')
+	sign *= -1;
+	if (s[i] >= '0' && s[i] <= '9')
+{
+	flag = 1;
+	result *= 10;
+	result += (s[i] - '0');
+}
+	else if (flag == 1)
+	flag = 2;
 	}
-
 	if (sign == -1)
-		output = -result;
+	output = -result;
 	else
-		output = result;
-
+	output = result;
 	return (output);
 }
 
